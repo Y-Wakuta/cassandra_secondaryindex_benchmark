0fc875d1e9e8ebe638de0fb608759c54a26e1a30
yusuke <prsprivatestock@i.softbank.jp>
2018-10-08 20:09:52 +0900

    check performance diff between no_index and with_index

--- cassandra/script/exec_stress_tool.sh
+++ cassandra/script/exec_stress_tool.sh
@@ -1,1 +1,15 @@
-cassandra-stress user profile=./blogpost.yaml n=100 ops\(insert=1,read=1\) no-warmup cl=QUORUM
+function recho { echo -e "\e[1;31m $* \e[m"; }
+
+ops_sum=10000
+file_name=./bench_graph.html
+
+cqlsh -e "DROP KEYSPACE IF EXISTS bench;"
+
+recho "no index"
+cassandra-stress user profile=./no_2nd_index.yaml n=$ops_sum ops\(insert=1,get_by_first_name=1\)  no-warmup cl=QUORUM -graph file=$file_name title=bench revision=no_index
+
+cqlsh -e "DROP KEYSPACE IF EXISTS bench;"
+recho "with index"
+cassandra-stress user profile=./with_2nd_index.yaml n=$ops_sum ops\(insert=1,get_by_first_name=1\)  no-warmup cl=QUORUM -graph file=$file_name title=bench revision=with_index
+
+cqlsh -e "DROP KEYSPACE IF EXISTS bench;"
