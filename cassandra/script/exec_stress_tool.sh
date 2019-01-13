4b3bb659edafc0fb1ae2cf635e755da21c19ead9
Y-Wakuta <prsprivatestock@i.softbank.jp>
2019-01-13 17:40:08 +0900

    on_server

---
+++ cassandra/script/exec_stress_tool.sh
@@ -0,0 +1,15 @@
+function recho { echo -e "\e[1;31m $* \e[m"; }
+function drop_keyspace { cqlsh -e "DROP KEYSPACE IF EXISTS $*;"; recho "$* dropped."; }
+
+ops_sum=1000000
+file_name=./bench_graph.html
+
+
+drop_keyspace "bench"
+recho "no index"
+cassandra-stress user profile=./no_2nd_index.yaml n=$ops_sum ops\(insert=3,get_by_first_name=1\) cl=QUORUM -graph file=$file_name title=bench revision=no_index
+
+drop_keyspace "bench"
+recho "with index"
+cassandra-stress user profile=./with_2nd_index.yaml n=$ops_sum ops\(insert=3,get_by_first_name=1\) cl=QUORUM -graph file=$file_name title=bench revision=with_index
+#drop_keyspace "bench"
--- cassandra/script/primary_key_vs_secondary_index/exec_exp_primary_key_vs_secondary_index.sh
+++ cassandra/script/primary_key_vs_secondary_index/exec_exp_primary_key_vs_secondary_index.sh
@@ -1,19 +1,28 @@
 function recho { echo -e "\e[1;31m $* \e[m"; }
 function drop_keyspace { cqlsh -e "DROP KEYSPACE IF EXISTS $*;"; recho "$* dropped."; }
 
-ops_sum=1000000
-file_name=./bench_primary_vs_secondary.html
-title=primary_key_vs_secondary_index
+#ops_sum=1000000
+insert_num=1000000
+ops_sum=10
+title=primary_key_vs_custom_secondary_index
 now=`date +%Y%m%d_%H-%M-%S`
 log_file_name="log_${now}.log"
+file_name="bench_primary_vs_secondary_$1.html"
 
-{
-drop_keyspace "bench" 
+
+drop_keyspace "bench"
+#recho "sparce secondary index"
+#cassandra-stress user profile=./with_sparce_custom_index.yaml n=$ops_sum ops\(insert=9,get_by_userid_4_2ndary_index=1\) cl=QUORUM -graph file=$file_name title=$title revision=sparce_secondary_index
+
+recho "insert record"
+cassandra-stress user profile=./with_prefix_custom_index.yaml n=$insert_num ops\(insert=1\) cl=QUORUM 
+
+recho "prefix secondary index"
+cassandra-stress user profile=./with_prefix_custom_index.yaml n=$ops_sum ops\(get_by_userid_4_2ndary_index=1\) cl=QUORUM -graph file=$file_name title=$title revision=prefix_secondary_index
+
+#recho "sparce secondary index"
+#cassandra-stress user profile=./with_sparce_custom_index.yaml n=$ops_sum ops\(get_by_userid_4_2ndary_index=1\) cl=QUORUM -graph file=$file_name title=$title revision=sparce_secondary_index
+
+#drop_keyspace "bench" 
 recho "primary index"
-cassandra-stress user profile=./with_primary_index.yaml n=$ops_sum ops\(insert=3,get_by_userid=1\) cl=QUORUM -graph file=$file_name title=$title revision=primary_index
-
-drop_keyspace "bench"
-recho "secondary index"
-cassandra-stress user profile=./with_2nd_index.yaml n=$ops_sum ops\(insert=3,get_by_userid_4_2ndary_index=1\) cl=QUORUM -graph file=$file_name title=$title revision=secondary_index
-drop_keyspace "bench"
-} > $log_file_name
+cassandra-stress user profile=./with_primary_index.yaml n=$ops_sum ops\(get_by_userid=1\) cl=QUORUM -graph file=$file_name title=$title revision=primary_index
